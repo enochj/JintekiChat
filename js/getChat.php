e7c76df4c5b298e470eecf51fdc3d95a88735a7e
enochj <enochjennison@gmail.com>
2016-05-13 16:17:58 -0500

    got the feed updating automatically

---
+++ js/getChat.php
@@ -0,0 +1,8 @@
+<?php
+$url = "http://www.jinteki.net/messages/general";
+
+$response = file_get_contents($url);
+
+echo $response;
+
+
--- js/script.js
+++ js/script.js
@@ -2,81 +2,35 @@
  * 
  */
 
-var URL = 'http://www.jinteki.net/messages/general';
-//var URL = 'http://www.w3schools.com/angular/customers.php';
-
-function createCORSRequest(method, url){
-    var xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr){
-        xhr.open(method, url, true);
-    } else if (typeof XDomainRequest != "undefined"){
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-    } else {
-        xhr = null;
-    }
-    return xhr;
-}
-
-var request = createCORSRequest("get", URL);
-if (request){
-    request.onload = function() {
-        // ...
-    };
-    request.onreadystatechange = handler;
-    request.send();
-}
-
-
+var URL = '../js/getChat.php';
 var app = angular.module('chatbox', ['ngResource']);
-/*angular.module('app.services', ['ngResource']).
-	value('version', '0.1').
-	factory('boxDB', function($resource) {
-		return $resource('URL', {
-			alt:'json',
-			appToken:'TOKEN',
-			q:'rock',
-			callback: 'JSON_CALLBACK'
-		},
-		{
-			get: {
-				method:'JSONP',
-				headers: {
-					'Content-Type': 'application/json',
-					'Accept': 'application/json'
-				}
-			}
-		});
-	});
 
-app = angular.module('chatbox');*/
-
-app.config(['$httpProvider', function($httpProvider) {
-    $httpProvider.defaults.useXDomain = true;
-    delete $httpProvider.defaults.headers.common['X-Requested-With'];
-}
-]);
-
-
-app.controller('myCtrl', function($scope, $http) {
+app.controller('myCtrl', function($scope, $http, $interval) {
 	$scope.message = "WTF";
 	$scope.firstName="John";
 	$scope.lastName="Doe";
+	
 	$http({
 		method: "GET",
 		url: URL
 	}).then(function(response) {
-		$scope.chatData = response.data.records;
+		$scope.chatData = response.data;
 		$scope.statusCode = response.status;
 		$scope.statustext = response.statustext;
 	}, function myError(response) {
-		$scope.chatData = "NO GOOD";
+		$scope.chatData = response;
 	});
 	
-	
-	//var url = $resource('http://www.jinteki.net/messages/general');
-	//url.get({}, function(response){
-	//	$scope.chatData = response.data;
-	//	alert("wowowwo");
-	//});
+	$interval(function() {
+		$http({
+			method: "GET",
+			url: URL
+		}).then(function(response) {
+			$scope.chatData = response.data;
+			$scope.statusCode = response.status;
+			$scope.statustext = response.statustext;
+		}, function myError(response) {
+			$scope.chatData = response;
+		});
+	}, 20000);	
 });
